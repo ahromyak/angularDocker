daa112d5fc6d49b34394ac83dd097c4c1e39aacf
deepwest83 <deepwest83@mail.ru>
2016-10-23 14:21:19 +0300

    moved sign logic and retreive emails to factory

---
+++ app/components/home/js/gapiService.js
@@ -0,0 +1,37 @@
+/**
+ * Created by deepwest83 on 10/23/2016.
+ */
+
+cjs.factory('gapiFactory', function ($q) {
+    var request;
+
+    function _loadEmails() {
+        var deferred = $q.defer();
+        gapi.client.load('gmail', 'v1', function () {
+            request = gapi.client.gmail.users.threads.list({
+                'userId': 'me'
+            });
+            request.execute(function (resp) {
+                deferred.resolve(resp.threads);
+            });
+        });
+        return deferred.promise;
+    }
+
+    function _signIn(signInCallback) {
+        gapi.signin.render('signInButton',
+            {
+                'callback': signInCallback, // Function handling the callback.
+                'clientid': '162151956695-3voa7h9ij4e2mqasv69g6sp6uouqsc29.apps.googleusercontent.com', // CLIENT_ID from developer console which has been explained earlier.
+                'requestvisibleactions': 'http://schemas.google.com/AddActivity', // Visible actions, scope and cookie policy wont be described now,
+                'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/gmail.readonly',
+                'cookiepolicy': 'single_host_origin'
+            }
+        );
+    };
+
+    return {
+        signIn: _signIn,
+        loadEmails: _loadEmails
+    }
+});
--- app/components/home/js/homeController.js
+++ app/components/home/js/homeController.js
@@ -1,13 +1,6 @@
-cjs.controller('homeController', function ($scope, $timeout) {
+cjs.controller('homeController', function ($scope, $timeout, gapiFactory) {
     // This flag we use to show or hide the button in our HTML.
     $scope.signedIn = false;
-
-    function listThreads(userId, callback) {
-        var request = gapi.client.gmail.users.threads.list({
-            'userId': userId
-        });
-        request.execute(callback);
-    };
 
     $scope.processAuth = function (authResult) {
         // Do a check if authentication has been successful.
@@ -18,33 +11,23 @@ cjs.controller('homeController', function ($scope, $timeout) {
         } else if (authResult['error']) {
             // Error while signing in.
             $scope.signedIn = false;
-            // Report error.
         }
     };
     // When callback is received, we need to process authentication.
     $scope.signInCallback = function (authResult) {
+        var promiseEmails;
         $scope.$apply(function () {
             $scope.processAuth(authResult);
         });
-        gapi.client.load('gmail', 'v1', function() {
-            listThreads('me', function(resp) {
-                $scope.$apply(function () {
-                    $scope.mailList = resp.threads;
-                });
-            });
+        //Retreive emails
+        promiseEmails = gapiFactory.loadEmails();
+        promiseEmails.then(function(data){
+                $scope.mailList = data;
         });
     };
     // Render the sign in button.
     $scope.renderSignInButton = function () {
-        gapi.signin.render('signInButton',
-            {
-                'callback': $scope.signInCallback, // Function handling the callback.
-                'clientid': '162151956695-3voa7h9ij4e2mqasv69g6sp6uouqsc29.apps.googleusercontent.com', // CLIENT_ID from developer console which has been explained earlier.
-                'requestvisibleactions': 'http://schemas.google.com/AddActivity', // Visible actions, scope and cookie policy wont be described now,
-                'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/gmail.readonly',
-                'cookiepolicy': 'single_host_origin'
-            }
-        );
+        gapiFactory.signIn($scope.signInCallback);
     };
     // Start function in this example only renders the sign in button.
     $scope.start = function () {
